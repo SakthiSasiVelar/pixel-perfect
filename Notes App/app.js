742d2ed1a143bb6bbb1d744f21f23b7201289a97
sakthi <sakthisasivelar@gmail.com>
2024-10-23 12:50:14 +0530

    cookie path change

--- Notes App/app.js
+++ Notes App/app.js
@@ -155,7 +155,8 @@ function checkUserLoggedIn(){
 }
 
 function removeCookie(name){
-    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    console.log('called')
+    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC;";
 }
 
 function showLoginContainer(){
